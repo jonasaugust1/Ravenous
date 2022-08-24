76b28fc25c33e9ea722a1e9c54afa6f9ffed12c1
Jonas Augusto <jonasaugusto99@gmail.com>
2022-08-24 09:58:10 -0300

    :sparkles: feat: Added Components

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar/>
+        <BusinessList/> 
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
